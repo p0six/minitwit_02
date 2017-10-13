9a39b2b685d108464f6a07f0b069f0b9fd4f1826
Michael Romero <romerom@gmail.com>
2017-10-13 02:59:20 -0700

    made the startup script a bit cleaner..

--- startup_flask.sh
+++ startup_flask.sh
@@ -1,19 +1,10 @@
 #!/bin/bash
-
-FLASK_APP=minitwit
-export FLASK_APP
-flask initdb && flask populatedb
-
-sleep 5
-
-MINITWIT_SETTINGS="`pwd`/sessions/session_store01.cfg"
-export MINITWIT_SETTINGS
-flask run -p 5000 &
-
-MINITWIT_SETTINGS="`pwd`/sessions/session_store02.cfg"
-export MINITWIT_SETTINGS
-flask run -p 5001 &
-
-MINITWIT_SETTINGS="`pwd`/sessions/session_store02.cfg"
-export MINITWIT_SETTINGS
-flask run -p 5002 &
+FLASK_APP=minitwit && export FLASK_APP
+flask initdb && flask populatedb && sleep 3
+for i in `echo 1 2 3`
+do
+  let "j = $i - 1"
+  MINITWIT_SETTINGS="`pwd`/sessions/session_store0${i}.cfg"
+  export MINITWIT_SETTINGS
+  flask run -p 500${j} &
+done
